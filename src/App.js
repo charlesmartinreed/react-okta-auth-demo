b496e423225c544fe96c219e8d4ce819159882b0
charlesmartinreed <charlesmartinreed@icloud.com>
2019-05-15 17:29:14 -0500

    Implement sign in widget for protected staff route using okta.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,42 @@ import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import Staff from "./components/pages/Staff";
+import Login from "./components/auth/Login";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Security, SecureRoute, ImplicitCallback } from "@okta/okta-react";
+
+// OKTA setup
+// Wrap everything in the Security component
+// setup the Security props
+// Use SecureRoute instead of Route component
+
+function onAuthRequired({ history }) {
+  history.push("/login");
+}
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Route exact path="/" component={Home} />
-          <Route exact path="/staff" component={Staff} />
+      <Security
+        issuer="https://dev-534664.okta.com/oauth2/default"
+        client_id="0oalpcxze5wNruN4c356"
+        redirect_uri={window.location.origin + "/implicit/callback"}
+        onAuthRequired={onAuthRequired}
+      >
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Route exact path="/" component={Home} />
+            <SecureRoute exact path="/staff" component={Staff} />
+            <Route
+              path="/login"
+              render={() => <Login baseUrl="https://dev-534664.okta.com" />}
+            />
+            <Route path="/implicit/callback" component={ImplicitCallback} />
+          </div>
         </div>
-      </div>
+      </Security>
     </Router>
   );
 }
---
+++ src/components/auth/SignInWidget.js
@@ -0,0 +1,25 @@
+// child component of the Login component
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import OktaSignIn from "@okta/okta-signin-widget";
+import "@okta/okta-signin-widget/dist/css/okta-sign-in.min.css";
+import "@okta/okta-signin-widget/dist/css/okta-theme.css";
+
+// success and error are handled in parent component, Login.js
+export default class SignInWidget extends Component {
+  componentDidMount() {
+    const el = ReactDOM.findDOMNode(this);
+    this.widget = new OktaSignIn({
+      baseUrl: this.props.baseUrl
+    });
+    this.widget.renderEl({ el }, this.props.onSuccess, this.props.onError);
+  }
+
+  componentWillUnmount() {
+    this.widget.remove();
+  }
+
+  render() {
+    return <div />;
+  }
+}
