6f8a5eed6a6de021d9d0be2721cbd834c871a0fb
charlesmartinreed <charlesmartinreed@icloud.com>
2019-05-15 17:51:16 -0500

    Create the Home component, using okta auth for account handling.

--- src/components/auth/SignInWidget.js
+++ src/components/auth/SignInWidget.js
@@ -6,11 +6,13 @@ import "@okta/okta-signin-widget/dist/css/okta-sign-in.min.css";
 import "@okta/okta-signin-widget/dist/css/okta-theme.css";
 
 // success and error are handled in parent component, Login.js
+// logo is kept in the public folder
 export default class SignInWidget extends Component {
   componentDidMount() {
     const el = ReactDOM.findDOMNode(this);
     this.widget = new OktaSignIn({
-      baseUrl: this.props.baseUrl
+      baseUrl: this.props.baseUrl,
+      logo: "logo.png"
     });
     this.widget.renderEl({ el }, this.props.onSuccess, this.props.onError);
   }
